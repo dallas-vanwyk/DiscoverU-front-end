4cbd2b005dc1bcde37072e51de3d9ca93595b95b
Zebastian Rodriguez <Zebo5914@gmail.com>
2025-06-06 20:18:39 -0400

    allows users to see the avatar on all pages

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,22 @@
 // src/components/HomePage.jsx
 
 import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const { user } = useContext(UserContext);
 
   return (
     <main>
+       {user?.avatar !== undefined && (
+        <img
+          src={`/images/avatars/${user.avatar}`}
+          alt="User Avatar"
+          style={{ width: "80px", borderRadius: "50%", marginBottom: "1rem" }}
+        />
+      )}
       <img src="images/main-app/home-page.svg" alt="Icon" />
       <br />
       <button onClick={() => navigate("/personality-test")}>
