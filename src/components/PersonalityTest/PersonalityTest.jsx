5189aa8fba8d8763eb90a020513acbdeda325243
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-06-07 17:52:06 -0500

    continuing styling

--- src/components/PersonalityTest/PersonalityTest.jsx
+++ src/components/PersonalityTest/PersonalityTest.jsx
@@ -1,19 +1,24 @@
 // src/components/PersonalityTest.jsx
 
+import NavBar from "../NavBar/NavBar.jsx";
+
 const HomePage = () => {
   return (
-    <main>
-      <p>Discover your inner traits</p>
-      <img src="images/main-app/personality-test.svg" alt="Icon" />
-      <a
-        href="https://www.yourselfirst.com/career?utm_source=google&utm_medium=cpc&utm_term=personality%20test%20for%20career%20path&utm_campaign=Careers&utm_campaign_name=Careers&utm_campaign_id=22078750217&utm_adset_id=181370381988&utm_adset_name=Careers&utm_ad_id=727401597295&gad_source=1&gad_campaignid=22078750217&gbraid=0AAAAA-F0voz2lEM0t6kx2KWXTlQCxfAQC&gclid=Cj0KCQjwiqbBBhCAARIsAJSfZkYYvWKzHwf2YsVdN6Wzo_7WhGONAmKznF-DwDBrzeZCBMMmE8rFQKUaAtN1EALw_wcB"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <br />
-        <button>Test It Out!</button>
-      </a>
-    </main>
+    <>
+      <main>
+        <p>Discover your inner traits</p>
+        <img src="images/main-app/personality-test.svg" alt="Icon" />
+        <a
+          href="https://www.yourselfirst.com/career?utm_source=google&utm_medium=cpc&utm_term=personality%20test%20for%20career%20path&utm_campaign=Careers&utm_campaign_name=Careers&utm_campaign_id=22078750217&utm_adset_id=181370381988&utm_adset_name=Careers&utm_ad_id=727401597295&gad_source=1&gad_campaignid=22078750217&gbraid=0AAAAA-F0voz2lEM0t6kx2KWXTlQCxfAQC&gclid=Cj0KCQjwiqbBBhCAARIsAJSfZkYYvWKzHwf2YsVdN6Wzo_7WhGONAmKznF-DwDBrzeZCBMMmE8rFQKUaAtN1EALw_wcB"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <br />
+          <button>Test It Out!</button>
+        </a>
+      </main>
+      <NavBar />
+    </>
   );
 };
 
