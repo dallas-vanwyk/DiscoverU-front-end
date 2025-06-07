127cc7e30a2d8b88e1576882ac50aee6a5d739c4
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-06-07 17:30:57 -0500

    disabled default app.css

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,27 +1,27 @@
 // src/components/HomePage.jsx
 
 import { useNavigate } from "react-router";
+import NavBar from "../NavBar/NavBar.jsx";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="homepage">
-      <img
-        src="images/main-app/home-page-crop.png"
-        alt="people discussing at table"
-        className="floating-content homepage-image"
-      />
-      <button onClick={() => navigate("/personality-test")}>
-        Personality Test
-      </button>
-      <button onClick={() => navigate("/")}>
-        Network
-      </button>
-      <button onClick={() => navigate("/city")}>
-        Career Professions
-      </button>
-    </main>
+    <>
+      <main className="homepage">
+        <img
+          src="images/main-app/home-page-crop.png"
+          alt="people discussing at table"
+          className="floating-content homepage-image"
+        />
+        <button onClick={() => navigate("/personality-test")}>
+          Personality Test
+        </button>
+        <button onClick={() => navigate("/")}>Network</button>
+        <button onClick={() => navigate("/city")}>Career Professions</button>
+      </main>
+      <NavBar />
+    </>
   );
 };
 
